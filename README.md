ef8a8ac42fe1f5be2b16697e8dc713248fc48e24
Ultimatepc1 <p.c90909@gmail.com>
2021-11-03 23:22:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-wdhbdf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-wdhbdf)
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 8080;
+//Creating general middleware for all routes to overide cors and help send response to any server
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //second param is name of domain
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // console.log('got request ')
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.json({ message: 'helloWorld' });
+});
+
+var listener = app.listen(PORT, () =>
+  console.log(`Server Running on Port: ${PORT}`)
+);
